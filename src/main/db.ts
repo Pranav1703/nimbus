b5fb8a2b1c90b8918978eb1553704dd997026d2e
Pranav1703 <pranavsai2005@gmail.com>
2025-02-10 20:01:56 +0530

    db url changed

--- src/main/db.ts
+++ src/main/db.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
+import "dotenv/config"
 
-const MONGO_URI = "mongodb://localhost:27017/nimbus";
+const MONGO_URI = process.env.DB || "mongodb://localhost:27017/nimbus";
 
 export const connectDB = async () => {
     try {
